ee559cdf28450c5d44e024da248e48be23334dfa
Jason <jie-email@jie-trancender.org>
2020-06-24 18:12:44 +0800

    refactor structure

---
+++ go_code/array/moveRight.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5}
+
+	for k, v := range moveRight(arr, 2) {
+		fmt.Printf("k = %d, v = %d\n", k, v)
+	}
+}
+
+// swap 两数交换
+func swap(a *int, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
+}
+
+// reverse 数组逆转
+func reverse(arr []int, begin, end int) {
+	for i := 0; i <= (end-begin)/2; i++ {
+		swap(&arr[begin+i], &arr[end-i])
+	}
+}
+
+// moveRight 数组右移k位
+func moveRight(arr []int, k int) []int {
+	len := len(arr)
+	reverse(arr, 0, len-1)
+	reverse(arr, 0, k-1)
+	reverse(arr, k, len-1)
+	return arr
+}
